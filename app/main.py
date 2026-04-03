1969d042647bcec2d0ca48f1d6afd7663d7cd415
taydongtruong <thailt089201@gmail.com>
2026-04-04 03:23:28 +0700

    mở cổng COR kết nối frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.api import api_router
 
 app = FastAPI(title="Fast Coffee Professional API")
+
+# CORS cho frontend local / deployed apps
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"] ,  # nếu cần tài nguyên bảo mật thì đổi thành ['http://localhost:3000']
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+)
 
 # Đăng ký Version 1
 app.include_router(api_router, prefix="/api/v1")
